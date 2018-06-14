d4d7736372aded493707e6a98683bcb5186b843f
David Lu <dlu777@gmail.com>
2018-06-14 15:58:43 -0500

    first committ

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,5 +1,5 @@
 const User = require('./user');
-const Player = require('.player');
+const Player = require('./player');
 
 /**
  * If we had any associations to make, this would be a great place to put them!
